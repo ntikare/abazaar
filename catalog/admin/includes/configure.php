29117f693e363adc2c2c49a343a6a566ff18482e
EC2 Default User <ec2-user@ip-10-146-74-254.ap-northeast-1.compute.internal>
2012-09-16 15:32:33 +0000

    change perm remote

--- catalog/admin/includes/configure.php
+++ catalog/admin/includes/configure.php
@@ -1,13 +1,13 @@
 <?php
-  define('HTTP_SERVER', 'http://localhost');
-  define('HTTP_CATALOG_SERVER', 'http://localhost');
-  define('HTTPS_CATALOG_SERVER', 'http://localhost');
+  define('HTTP_SERVER', 'http://nawa.ibhupi.com');
+  define('HTTP_CATALOG_SERVER', 'http://nawa.ibhupi.com');
+  define('HTTPS_CATALOG_SERVER', 'http://nawa.ibhupi.com');
   define('ENABLE_SSL_CATALOG', 'false');
-  define('DIR_FS_DOCUMENT_ROOT', 'C:/wamp/www/shop/catalog/');
-  define('DIR_WS_ADMIN', '/shop/catalog/admin/');
-  define('DIR_FS_ADMIN', 'C:/wamp/www/shop/catalog/admin/');
-  define('DIR_WS_CATALOG', '/shop/catalog/');
-  define('DIR_FS_CATALOG', 'C:/wamp/www/shop/catalog/');
+  define('DIR_FS_DOCUMENT_ROOT', '/var/www/html/abazaar/catalog/');
+  define('DIR_WS_ADMIN', '/abazaar/catalog/admin/');
+  define('DIR_FS_ADMIN', '/var/www/html/abazaar/catalog/admin/');
+  define('DIR_WS_CATALOG', '/abazaar/catalog/');
+  define('DIR_FS_CATALOG', '/var/www/html/abazaar/catalog/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
@@ -27,7 +27,7 @@
 
   define('DB_SERVER', 'localhost');
   define('DB_SERVER_USERNAME', 'root');
-  define('DB_SERVER_PASSWORD', 'nnt123');
+  define('DB_SERVER_PASSWORD', 'savvanavu');
   define('DB_DATABASE', 'asianbazaar');
   define('USE_PCONNECT', 'false');
   define('STORE_SESSIONS', 'mysql');
