e1f097c07f83564152a7356c3220c035e1239b2b
navneet <navneet.tikare@gmail.com>
2012-10-22 19:25:42 +0900

    links module

---
+++ includes/languages/japanese/modules/boxes/bm_link.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ $Id$
+
+ osCommerce, Open Source E-Commerce Solutions
+ http://www.oscommerce.com
+
+ Copyright (c) 2010 osCommerce
+
+ Released under the GNU General Public License
+ */
+
+define('MODULE_BOXES_LINK_TITLE', 'Links');
+define('MODULE_BOXES_LINK_DESCRIPTION', 'Show links ');
+define('MODULE_BOXES_LINK_BOX_TITLE', 'Links');
+define('MODULE_BOXES_LINK_BOX_SHOP_INFO', 'Shop Information');
+define('MODULE_BOXES_LINK_BOX_FACEBOOK', 'Asian Bazaar Facebook');
+//define('MODULE_BOXES_LINK_BOX_SHIPPING', 'About Shipping');
+//define('MODULE_BOXES_LINK_BOX_CONTACT', 'Contact Us');
+?>
